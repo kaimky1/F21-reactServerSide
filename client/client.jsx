c3980780a41665e6a510a36db1f3b944b43319b7
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-06-02 19:58:16 -1000

    created map functions and added data

--- client/client.jsx
+++ client/client.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { App } from './App'
 
-const App = () => (
-    <div>
-        This is a react component.
-    </div>
-)
 
 ReactDOM.render(<App></App>, document.querySelector('#container'))
